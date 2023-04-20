56fa956d0d84cf8c206313ec276b082685b630f7
Manuel <mriverospolo@gmail.com>
2023-04-20 09:28:18 -0500

    version v3.1

--- listar.php
+++ listar.php
@@ -4,14 +4,46 @@ $con = conexion();
 
 $sql = "select * from persona";
 $resultado = pg_query($con, $sql);
-
-while ($fila = pg_fetch_assoc($resultado)) {
-    echo "Documento: " . $fila['documento'] . "<br>";
-    echo "Nombre: " . $fila['nombre'] . "<br>";
-    echo "Apellido: " . $fila['apellido'] . "<br>";
-    echo "Dirección: " . $fila['direccion'] . "<br>";
-    echo "Celular: " . $fila['celular'] . "<br><br>";
-}
-
-pg_close($con);
 ?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Listado de Personas</title>
+	
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css">
+</head>
+<body>
+	<div class="container">
+		<h1>Listado de Personas</h1>
+		<table class="table table-striped">
+			<thead>
+				<tr>
+					<th>ID</th>
+					<th>Documento</th>
+					<th>Nombre</th>
+					<th>Apellido</th>
+					<th>Dirección</th>
+					<th>Celular</th>
+				</tr>
+			</thead>
+			<tbody>
+				<?php while($fila = pg_fetch_assoc($resultado)) { ?>
+					<tr>
+						<td><?php echo $fila['id']; ?></td>
+						<td><?php echo $fila['documento']; ?></td>
+						<td><?php echo $fila['nombre']; ?></td>
+						<td><?php echo $fila['apellido']; ?></td>
+						<td><?php echo $fila['direccion']; ?></td>
+						<td><?php echo $fila['celular']; ?></td>
+					</tr>
+				<?php } ?>
+			</tbody>
+		</table>
+	</div>
+	<!-- Agregamos los scripts de Bootstrap -->
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+	<script src="https://code.jquery.com/jquery-3.3.1.min.js"></script>
+</body>
+</html>
